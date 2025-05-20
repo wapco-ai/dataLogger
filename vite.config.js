bea13a0780a199958a1d381edb64b4d51405cdb8
gisonline <oskooei@gmail.com>
2025-05-20 13:36:14 +0330

    address

--- vite.config.js
+++ vite.config.js
@@ -31,12 +31,12 @@ export default defineConfig({
         start_url: '/dataLogger/',
         icons: [
           {
-            src: 'icon-192x192.png',
+            src: '/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512x512.png',
+            src: '/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
